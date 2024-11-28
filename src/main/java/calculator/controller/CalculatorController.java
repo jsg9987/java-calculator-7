7eef03a4589e1f076eaa16b89626db2ae7281a4e
jedo5000 <jsg998877@gmail.com>
2024-11-28 16:31:48 +0900

    feat: 문자열 입력 메소드 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,15 +1,16 @@
 package calculator.controller;
 
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
-
+//    private final SeparatorService separatorService;
     public void run() {
         getInputStr();
     }
 
     private void getInputStr() {
         OutputView.printNumInputPrompt();
-
+        String inputStr = InputView.getInputString();
     }
 }
