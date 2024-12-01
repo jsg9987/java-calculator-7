fbcf18558c4c8d1e5ebb86a735a96c3c3f74fcfc
전석균 <jsg998877@gmail.com>
2024-12-01 16:13:15 +0900

    feat($OutputView.java): add printErrorMessage method
    - 에러 메세지를 출력해주는 메소드를 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -3,8 +3,11 @@ package calculator.view;
 import calculator.constants.OutputPrompts;
 
 public class OutputView {
-
     public static void printNumInputPrompt() {
         System.out.println(OutputPrompts.NUM_INPUT_PROMPT.getPrompt());
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
     }
 }
